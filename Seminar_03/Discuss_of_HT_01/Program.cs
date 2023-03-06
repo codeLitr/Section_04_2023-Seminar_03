f7a070d71a9b99a952e67bc5adfdb63527ea766b
SMV <kraft088@gmail.com>
2023-03-06 21:49:56 +1100

    Add discussion of homework

---
+++ Seminar_03/Discuss_of_HT_01/Program.cs
@@ -0,0 +1,41 @@
+﻿// Обсуждение домашнего задания к Семинару №1:
+
+// Задача 2:
+// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+// a = 5;	b = 7 -> max 7
+// a = 2;	b = 10 -> max 10
+// a = -9;	b = -3 -> max -3
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int firstNumb = Prompt("Введите первое число > ");
+int secondNumb = Prompt("Введите второе число > ");
+
+// Предложение преподавателя сделать через библиотеку Math и выбрать подраздел Max -
+// это позволит выбрать из двух чисел наибольшее (мой вариант решения закомментирован):
+
+// if (firstNumb > secondNumb)
+// {
+// 	Console.Write("Наибольшее число -> ");
+//     Console.WriteLine(firstNumb);
+//     Console.Write("Наименьшее число -> ");
+//     Console.Write(secondNumb);
+// }
+// else
+// {
+//     Console.Write("Наименьшее число -> ");
+//     Console.WriteLine(firstNumb);
+//     Console.Write("Наибольшее число -> ");
+//     Console.Write(secondNumb);
+// }
+
+int max = Math.Max(firstNumb, secondNumb);
+System.Console.WriteLine($"Наибольшее число -> {max}");
+
+int min = Math.Min(firstNumb, secondNumb);
+System.Console.WriteLine($"Наименьшее число -> {min}");
---
+++ Seminar_03/Discuss_of_HT_03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Обсуждение домашнего задания к Семинару №1:
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли
+// число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// По данной задаче предложений нет.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int A = Prompt("Введите число для проверки > ");
+
+if(A % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+}
