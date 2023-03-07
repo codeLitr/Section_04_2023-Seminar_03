8c3bb5034245ffe8a4a13593c2c0ab422724ab89
SMV <kraft088@gmail.com>
2023-03-07 15:15:58 +1100

    Add Task 2

--- Seminar_03/Task_02/Program.cs
+++ Seminar_03/Task_02/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hel33333lo, World!");
+﻿// Задача 2.
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных 
+// координат точек в этой четверти (x и y).
+
+System.Console.WriteLine("Введите число четверти: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Результат: ");
+if (a == 1)
+{
+    System.Console.WriteLine("x > 0, y > 0");
+}
+else if (a == 2)
+{
+    System.Console.WriteLine("x < 0, y > 0");
+}
+else if (a == 3)
+{
+    System.Console.WriteLine("x < 0, y < 0");
+}
+else if (a == 4)
+{
+    System.Console.WriteLine("x > 0, y < 0");
+}
+else
+{
+    System.Console.WriteLine("Неправильный ввод");
+}
